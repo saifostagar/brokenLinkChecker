5ee7fb0f8316b70f55f7b3e10e002d0f0ff94492
Saif Ostagar <saifostagar@gmail.com>
2024-03-20 19:17:47 +0530

    added auto email after crawling functionality

--- broken_links/pipelines.py
+++ broken_links/pipelines.py
@@ -6,7 +6,9 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-
+import smtplib
+from email.message import EmailMessage
+import config
 
 class BrokenLinksPipeline:
     def process_item(self, item, spider):
@@ -23,6 +25,8 @@ class SeparateFilePipeline:
         
         self.csv_writers = {}
 
+        self.files_name=[]
+
     @classmethod
     def from_crawler(cls, crawler):
         return cls(crawler.spider.name)
@@ -31,8 +35,32 @@ class SeparateFilePipeline:
         pass
 
     def close_spider(self, spider):
-        for csv_file in self.csv_writers.values():
-            csv_file.close()
+        self.send_mail()
+
+    def send_mail(self):
+    
+        try:
+            msg = EmailMessage()
+            msg['From'] = config.EMAIL_USER
+            msg['To'] = config.EMAIL_TO
+            msg['Subject'] = "Report of Broken Links"
+            msg.set_content("This is a Test Mail. This is Mail Body")
+            for file_name in self.files_name:
+                with open(file_name, 'r') as f:
+                    data= f.read()
+                msg.add_attachment(data, filename = file_name)
+
+            server = smtplib.SMTP('smtp.gmail.com',587)
+            server.starttls()
+            server.login(config.EMAIL_USER, config.EMAIL_PASS)
+            server.send_message(msg)
+            server.quit()
+            print("Email sent sucessfully")
+        except Exception as e:
+            print(f"An error occurred while sending the email: {e}")
+
+
+
 
     def process_item(self, item, spider):
 
@@ -49,6 +77,7 @@ class SeparateFilePipeline:
 
             if site_name not in self.csv_writers:
                 filename = f"{site_name}_Broken_Img_{datetime.datetime.now().strftime('%Y%m%d')}.csv"
+                self.files_name.append(filename)
                 csv_file = open(filename, 'w', newline='', encoding='utf-8')
                 fieldnames = ['Source_Page', 'Image_Link', 'HTTP_Code', 'Missing Alt', 'Alt Text']
                 writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
@@ -59,6 +88,7 @@ class SeparateFilePipeline:
             
             if site_name not in self.csv_writers:
                 filename = f"{site_name}_Broken_Links_{datetime.datetime.now().strftime('%Y%m%d')}.csv"
+                self.files_name.append(filename)
                 csv_file = open(filename, 'w', newline='', encoding='utf-8')
                 fieldnames = ['Source_Page', 'Link_Text', 'Broken_Page_Link', 'HTTP_Code', 'External']
                 writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
