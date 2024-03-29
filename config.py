087e29780a3f94b0a4a105b434e8e57b3869ffd2
Saif Ostagar <saifostagar@gmail.com>
2024-03-29 16:50:48 +0530

    edit config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 
-EMAIL_USER = os.environ.get('SCRIPT_EMAIL')
-EMAIL_PASS = os.environ.get('SCRIPT_PASS') # create your apppassword from here https://myaccount.google.com/apppasswords
+EMAIL_USER = os.getenv('SCRIPT_EMAIL')
+EMAIL_PASS = os.getenv('SCRIPT_PASS') # create your apppassword from here https://myaccount.google.com/apppasswords
 EMAIL_TO = 'saifostagar@gmail.com'
+print(EMAIL_USER)
