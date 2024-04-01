ecfdc5cf51970b96eb4d92a823a0cb7ba6f35e6d
Saif Ostagar <saifostagar@gmail.com>
2024-04-01 15:53:43 +0530

    modified pipelines.py

--- broken_links/pipelines.py
+++ broken_links/pipelines.py
@@ -50,8 +50,7 @@ class SeparateFilePipeline:
             msg.add_attachment(data, filename=file_name)
         
         try:
-            with smtplib.SMTP('smtp.gmail.com', 587) as server:
-                server.starttls()
+            with smtplib.SMTP_SSL('smtp.hostinger.com', 465) as server:
                 server.login(config.EMAIL_USER, config.EMAIL_PASS)
                 server.send_message(msg)
                 server.quit()
