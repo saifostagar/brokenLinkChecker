d695859ec9c9cdeeca7c9f7229254f2a385f1ef6
Saif Ostagar <saifostagar@gmail.com>
2024-04-01 12:42:34 +0530

    modified smtp settings

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
 
 EMAIL_USER = os.getenv('SCRIPT_EMAIL')
-EMAIL_PASS = os.getenv('SCRIPT_PASS') # create your apppassword from here https://myaccount.google.com/apppasswords
+EMAIL_PASS = os.getenv('SCRIPT_PASS') # create your apppassword from here https://myaccount.google.com/apppasswords for gmail
+SMTP_HOST = os.getenv('SMTP_HOST')
+SMTP_PORT= os.getenv('SMTP_PORT')
 EMAIL_TO = 'saifostagar@gmail.com'
 print(EMAIL_USER)
