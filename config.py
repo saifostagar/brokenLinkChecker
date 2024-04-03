d2173a689cf9a1000b690f255ee24912b50109bd
Saif Ostagar <saifostagar@gmail.com>
2024-04-03 22:02:33 +0530

    change mail recipients

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 EMAIL_USER = os.getenv('SCRIPT_EMAIL')
 EMAIL_PASS = os.getenv('SCRIPT_PASS') # create your apppassword from here https://myaccount.google.com/apppasswords for gmail
-EMAIL_TO = 'saifostagar@gmail.com'
-print(EMAIL_USER)
+EMAIL_TO = 'md.saif@shorthillstech.com, deepa@shorthillstech.com'
+print('Email will be sent to '+EMAIL_TO)
